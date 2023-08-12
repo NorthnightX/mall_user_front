f88100828031e7d6f35dc710ecee0f48b726c23e
zhangjize <z15588222448@gmail.com>
2023-08-12 16:22:14 +0800

    修改，删除博客

--- src/views/editor/blogEditor.vue
+++ src/views/editor/blogEditor.vue
@@ -42,7 +42,7 @@
               </div>
             </div>
 
-            <div style="width: 80%; min-height: 580px; height: auto; margin: 0 3%" v-show="!editorBLog">
+            <div style="width: 80%; min-height: 580px; height: auto; margin: 0 3%" v-show="!editorBLog && !updateBLogVisible">
               <div style="margin-top: 15px;width: 100%;display: flex;justify-content: space-between;">
                 <div style="width: 30%;">
                   <span style="font-size: 13px">标题</span>
@@ -88,7 +88,7 @@
                           <el-button type="text" size="small" @click="updateBLog(blog)">编辑</el-button>
                         </div>
                         <div style="width: 10%; align-items: center; display: flex; justify-content: center;">
-                          <el-button type="text" size="small" @click="deleteBlog()">删除</el-button>
+                          <el-button type="text" size="small" @click="deleteBlog(blog.id)">删除</el-button>
                         </div>
                       </div>
                     </div>
@@ -164,6 +164,70 @@
                 </div>
               </div>
             </div>
+<!--            修改区-->
+            <div style="width: 80%; margin: 0 3%" v-show="updateBLogVisible">
+              <div style="margin-top: 1%">
+                <el-form>
+                  <el-form-item label="标题" prop="title">
+                    <el-input v-model="updateBlogForm.title"></el-input>
+                  </el-form-item>
+                </el-form>
+              </div>
+              <div style="margin-top: 10px;">
+                <span style="color: #666666">内容</span>
+                <el-form style="margin-top: 10px">
+                  <el-form-item prop="content">
+                    <mavon-editor
+                        v-model="updateBlogForm.content"
+                        ref=md
+                        @imgAdd="$imgAdd"></mavon-editor>
+                  </el-form-item>
+                </el-form>
+              </div>
+              <div style="margin-top: 10px; background-color: whitesmoke">
+                <div style="background-color: cornflowerblue">
+                  <span style="color: #666666">分类</span>
+                  <el-button style="margin-left: 20px;color: gainsboro" size="medium" type="text"
+                             @click="addBlogType()">+ 新建分类
+                  </el-button>
+                </div>
+                <el-form style="margin-top: 10px">
+                  <el-form-item prop="content">
+                    <el-select v-model="updateBlogForm.typeId" v-show="types.length > 0" placeholder="请选择分类">
+                      <el-option
+                          v-for="type in types"
+                          :key=type.id
+                          :label=type.name
+                          :value=type.id>
+                      </el-option>
+                    </el-select>
+                    <el-select disabled v-show="types.length === 0" placeholder="还没有分类哦，新建一个吧">
+                    </el-select>
+                  </el-form-item>
+                </el-form>
+              </div>
+              <div style="margin-top: 10px; background-color: whitesmoke">
+                <div
+                    style="background-color: cornflowerblue; height: 35px; justify-content: left;display: flex;align-items: center">
+                  <span style="color: #666666">高级选项</span>
+                </div>
+                <div>
+                  <template>
+                    <el-radio v-model="updateBlogForm.isPublic" :label=1>公开</el-radio>
+                    <el-radio v-model="updateBlogForm.isPublic" :label=2>只有我</el-radio>
+                  </template>
+                </div>
+              </div>
+              <div style="justify-content: left; display: flex; align-items: center; height: 50px">
+                <div>
+                  <el-button type="primary" style="background-color: #3366cc;" size="medium" @click="submitUpdateForm()">
+                    提交
+                  </el-button>
+                  <el-button type="primary" style="background-color: #3366cc;" size="medium" @click="clearForm()">取消
+                  </el-button>
+                </div>
+              </div>
+            </div>
           </div>
         </div>
         <!-- 页面底部 -->
@@ -206,6 +270,7 @@ export default {
   data() {
     return {
       blogs: [],
+      updateBLogVisible: false,
       editorBLog: false,
       user: {},
       editAddTypeForm: {
@@ -227,6 +292,13 @@ export default {
         blogger: '',
         isPublic: 1
       },
+      updateBlogForm: {
+        content: '',
+        title: '',
+        typeId: '',
+        blogger: '',
+        isPublic: 1
+      },
     };
   },
   computed: {},
@@ -235,11 +307,19 @@ export default {
       this.editorBLog = true
     },
     updateBLog(blog){
-      this.guideDetail = {...blog}
-      this.editorBLog = true
+      this.updateBlogForm = {...blog}
+      this.updateBLogVisible = true
     },
-    deleteBlog(){
+    deleteBlog(id){
+      const confirmed = confirm('确认要取消吗？');
+      if (confirmed) {
+        console.log(id)
 
+      }
+    },
+    submitUpdateForm(){
+      console.log(this.updateBlogForm)
+      this.updateBLogVisible = false
     },
     submitAddForm() {
       const user = JSON.parse(sessionStorage.getItem("token"));
@@ -275,6 +355,7 @@ export default {
         this.guideDetail.blogger = '';
         this.guideDetail.isPublic = 1;
         this.editorBLog = false
+        this.updateBLogVisible = false
       }
     },
     $imgAdd(pos, $file) {
