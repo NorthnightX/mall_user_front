f637446c6d120e6eaaf615aa9a7d422302ff860c
zhangjize <z15588222448@gmail.com>
2023-08-12 16:09:02 +0800

    个人博客编辑界面

--- src/views/editor/blogEditor.vue
+++ src/views/editor/blogEditor.vue
@@ -36,11 +36,72 @@
         <div style="width: 100%">
           <div style="display: flex; justify-content: space-between;">
             <!-- 编辑区的右侧工具栏 -->
-            <div style="width: 13%; background-color: whitesmoke; margin-right: 10px">
-
+            <div style="width: 13%; background-color: whitesmoke; margin-right: 10px;">
+              <div style="width: 100%;justify-content: center;align-items: center;display:flex;">
+                <el-button type="text" @click="addBLog()">新建博客</el-button>
+              </div>
             </div>
+
+            <div style="width: 80%; min-height: 580px; height: auto; margin: 0 3%" v-show="!editorBLog">
+              <div style="margin-top: 15px;width: 100%;display: flex;justify-content: space-between;">
+                <div style="width: 30%;">
+                  <span style="font-size: 13px">标题</span>
+                </div>
+                <div style="display: flex; width: 70%;margin-left: 30%">
+                  <div style="width: 20%;align-items: center;display: flex;justify-content: center">
+                    <span style="font-size: 13px">发布时间</span>
+                  </div>
+                  <div style="width: 10%;align-items: center;display: flex;justify-content: center">
+                    <span style="font-size: 13px">评论数</span>
+                  </div>
+                  <div style="width: 10%;align-items: center;display: flex;justify-content: center">
+                    <span style="font-size: 13px">点击量</span>
+                  </div>
+                  <div style="width: 10%;align-items: center;display: flex;justify-content: center">
+                    <span style="font-size: 13px">操作</span>
+                  </div>
+                  <div style="width: 10%;align-items: center;display: flex;justify-content: center">
+                    <span style="font-size: 13px">删除</span>
+                  </div>
+                </div>
+              </div>
+              <div style="height: 1px;width: 100%;background-color: cornflowerblue;margin-top: 10px"></div>
+              <div>
+                <div>
+                  <div v-for="(blog, index) in blogs" :key="blog.id">
+                    <div :style="{ display: 'flex', width: '100%', height: '35px', alignItems: 'center',
+                    justifyContent: 'space-between', backgroundColor: index % 2 === 1 ? '#f7f7f7' : 'transparent' }">
+                      <div class="blogTitle" style="width: 30%;">
+                        <span style="font-size: 12px">{{ blog.title }}</span>
+                      </div>
+                      <div style="display: flex; width: 70%;margin-left: 30%">
+                        <div style="width: 20%; align-items: center; display: flex; justify-content: center;">
+                          <span style="font-size: 12px">{{ blog.gmtCreate }}</span>
+                        </div>
+                        <div style="width: 10%; align-items: center; display: flex; justify-content: center;">
+                          <span style="font-size: 12px">{{ blog.comment }}</span>
+                        </div>
+                        <div style="width: 10%; align-items: center; display: flex; justify-content: center;">
+                          <span style="font-size: 12px">{{ blog.clickCount }}</span>
+                        </div>
+                        <div style="width: 10%; align-items: center; display: flex; justify-content: center;">
+                          <el-button type="text" size="small" @click="updateBLog(blog)">编辑</el-button>
+                        </div>
+                        <div style="width: 10%; align-items: center; display: flex; justify-content: center;">
+                          <el-button type="text" size="small" @click="deleteBlog()">删除</el-button>
+                        </div>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+
+
+              </div>
+            </div>
+
+            <div></div>
             <!-- 编辑区 -->
-            <div style="width: 80%; margin: 0 3%">
+            <div style="width: 80%; margin: 0 3%" v-show="editorBLog">
               <div style="margin-top: 1%">
                 <el-form>
                   <el-form-item label="标题" prop="title">
@@ -62,7 +123,9 @@
               <div style="margin-top: 10px; background-color: whitesmoke">
                 <div style="background-color: cornflowerblue">
                   <span style="color: #666666">分类</span>
-                  <el-button style="margin-left: 20px;color: gainsboro" size="medium" type="text" @click="addBlogType()">+ 新建分类</el-button>
+                  <el-button style="margin-left: 20px;color: gainsboro" size="medium" type="text"
+                             @click="addBlogType()">+ 新建分类
+                  </el-button>
                 </div>
                 <el-form style="margin-top: 10px">
                   <el-form-item prop="content">
@@ -86,8 +149,8 @@
                 </div>
                 <div>
                   <template>
-                    <el-radio v-model="guideDetail.isPublic" label="1">公开</el-radio>
-                    <el-radio v-model="guideDetail.isPublic" label="2">只有我</el-radio>
+                    <el-radio v-model="guideDetail.isPublic" :label=1>公开</el-radio>
+                    <el-radio v-model="guideDetail.isPublic" :label=2>只有我</el-radio>
                   </template>
                 </div>
               </div>
@@ -96,7 +159,8 @@
                   <el-button type="primary" style="background-color: #3366cc;" size="medium" @click="submitForm()">
                     提交
                   </el-button>
-                  <el-button type="primary" style="background-color: #3366cc;" size="medium" @click="clearForm()">取消</el-button>
+                  <el-button type="primary" style="background-color: #3366cc;" size="medium" @click="clearForm()">取消
+                  </el-button>
                 </div>
               </div>
             </div>
@@ -111,21 +175,23 @@
             </div>
           </div>
         </div>
-        <el-dialog  :visible.sync="addTypeFormVisible" width="30%" @click='closeDialog("edit")'>
+        <el-dialog :visible.sync="addTypeFormVisible" width="30%" @click='closeDialog("edit")'>
           <el-form label-width="80px" ref="editAddTypeForm" :model="editAddTypeForm" :rules="rules">
             <el-form-item label="名称" prop="name">
-              <el-input size="small" v-model="editAddTypeForm.name" auto-complete="off" placeholder="请输入分类名称"></el-input>
+              <el-input size="small" v-model="editAddTypeForm.name" auto-complete="off"
+                        placeholder="请输入分类名称"></el-input>
             </el-form-item>
             <el-form-item label="可见性" prop="isPublic">
               <template>
-                <el-radio v-model="editAddTypeForm.isPublic" label="1">公开</el-radio>
-                <el-radio v-model="editAddTypeForm.isPublic" label="2">只有我</el-radio>
+                <el-radio v-model="editAddTypeForm.isPublic" :label=1>公开</el-radio>
+                <el-radio v-model="editAddTypeForm.isPublic" :label=2>只有我</el-radio>
               </template>
             </el-form-item>
           </el-form>
           <div slot="footer" class="dialog-footer">
             <el-button size="small" @click='closeDialog("edit")'>取消</el-button>
-            <el-button size="small" type="primary" class="title" @click="submitAddForm('editAddTypeForm')">保存</el-button>
+            <el-button size="small" type="primary" class="title" @click="submitAddForm('editAddTypeForm')">保存
+            </el-button>
           </div>
         </el-dialog>
       </div>
@@ -139,11 +205,13 @@ export default {
   name: "BlogApp",
   data() {
     return {
-      user:{},
-      editAddTypeForm : {
-        name:"",
-        isPublic: '1',
-        blogger : ''
+      blogs: [],
+      editorBLog: false,
+      user: {},
+      editAddTypeForm: {
+        name: "",
+        isPublic: 1,
+        blogger: ''
       },
       addTypeFormVisible: false,
       types: [],
@@ -157,45 +225,56 @@ export default {
         title: '',
         typeId: '',
         blogger: '',
-        isPublic: '1'
+        isPublic: 1
       },
     };
   },
   computed: {},
   methods: {
-    submitAddForm(){
+    addBLog(){
+      this.editorBLog = true
+    },
+    updateBLog(blog){
+      this.guideDetail = {...blog}
+      this.editorBLog = true
+    },
+    deleteBlog(){
+
+    },
+    submitAddForm() {
       const user = JSON.parse(sessionStorage.getItem("token"));
       this.editAddTypeForm.blogger = user.id
       this.$axios.post("/blogType/addType", this.editAddTypeForm).then(res => {
-        if(res.data.code === 200){
+        if (res.data.code === 200) {
           this.selectTypeByLoginUser()
           this.$message.success("保存成功")
           this.addTypeFormVisible = false
           this.editAddTypeForm.name = ''
-        }
-        else {
+        } else {
           this.$message.warning("网络异常")
         }
       })
     },
-    addBlogType(){
+    addBlogType() {
       this.addTypeFormVisible = true
     },
-    toHome(){
-      this.$router.push({ path: '/blog/home'});
+    toHome() {
+      this.$router.push({path: '/blog/home'});
     },
-    closeDialog(){
+    closeDialog() {
       this.addTypeFormVisible = false
       this.editAddTypeForm.name = ''
+      this.editAddTypeForm.isPublic = 1
     },
     clearForm() {
-      const confirmed = confirm('确认要清空吗？');
+      const confirmed = confirm('确认要取消吗？');
       if (confirmed) {
         this.guideDetail.content = '';
         this.guideDetail.title = '';
         this.guideDetail.typeId = '';
         this.guideDetail.blogger = '';
-        this.guideDetail.isPublic = '1';
+        this.guideDetail.isPublic = 1;
+        this.editorBLog = false
       }
     },
     $imgAdd(pos, $file) {
@@ -205,8 +284,7 @@ export default {
         if (res.status === 200) {
           var url = res.data.data;
           this.$refs.md.$img2Url(pos, url)
-        }
-        else {
+        } else {
           this.$message("网络异常")
         }
       })
@@ -221,7 +299,8 @@ export default {
           this.guideDetail.title = '';
           this.guideDetail.typeId = '';
           this.guideDetail.blogger = '';
-          this.guideDetail.isPublic = '1';
+          this.guideDetail.isPublic = 1;
+          this.editorBLog = false
         } else {
           this.$message.warning(res.data.data)
         }
@@ -236,10 +315,21 @@ export default {
           this.$message.warning(res.data.data)
         }
       })
+    },
+    queryBlogByUser(id) {
+      this.$axios.get("blog/blogByUser/" + id).then(res => {
+        if (res.data.code === 200) {
+          this.blogs = res.data.data;
+          this.loading = true
+        } else {
+          this.$message.warning("网络异常")
+        }
+      })
     }
   },
   created() {
     this.user = JSON.parse(sessionStorage.getItem("token"))
+    this.queryBlogByUser(this.user.id)
     this.selectTypeByLoginUser()
   }
 };
